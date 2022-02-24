42541c9a1a7e52bd069bdcf93c1258d062570245
Samsonova Yulia <samsonovajuls@gmail.com>
2022-02-24 22:31:33 +0200

    homework 1.2 is done

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ProfileView: UIView {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -19,15 +26,16 @@ class ProfileView: UIView {
         self.setupView()
     }
 
+
     private func setupView() {
         let view = self.loadViewFromXib()
-        view.frame = self.bounds // здесь view, загруженная из xib-файла, добавляется на вьюху класса. Ее размер соответствует размеру вьюхе класса и растягивается так же, как и сама вьюха класса.
+        view.frame = self.bounds
         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         self.addSubview(view)
     }
 
     private func loadViewFromXib() -> UIView {
-        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView else { return UIView() }
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)?.first as? UIView else { return UIView() }
 
         return view
     }
